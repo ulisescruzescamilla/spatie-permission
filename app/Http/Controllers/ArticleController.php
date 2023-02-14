d4821d1c586f311b3b7e8fa93db134270b4bdef9
Ulises Escamilla <desarrollo3@orbeseguros.com.mx>
2023-02-14 14:00:18 -0600

    Add edit article policy

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EditArticlesRequest;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function edit(EditArticlesRequest $request)
+    {
+        # edit article
+    }
+}
